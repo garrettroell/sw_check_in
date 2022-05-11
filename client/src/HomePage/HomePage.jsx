cb928e0fec4f04cfdeaf0be2fb853e62c11656b3
garrettroell <garrettroell@gmail.com>
2022-05-11 10:00:14 -0500

    removed unneeded images

--- client/src/HomePage/HomePage.jsx
+++ client/src/HomePage/HomePage.jsx
@@ -1,17 +1,9 @@
-import { Box, useColorMode } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { Box } from "@chakra-ui/react";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import FlightForm from "./FlightForm";
 
 const HomePage = () => {
-  const { setColorMode } = useColorMode()
-
-  // set inital color mode
-  useEffect(() => {
-    setColorMode('dark')
-  }, []);
-
   return ( 
     <>
       <Box w="100vw" h="100vh" positon="relative" px="20px">
