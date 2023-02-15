a49f4b296494f6a32a0410a97ccd881e46a59719
garrettroell <garrettroell@gmail.com>
2023-02-14 18:17:26 -0600

    made text area of upcoming flights page wider

--- client/src/UpcomingFlightsPage/UpcomingFlightsPage.jsx
+++ client/src/UpcomingFlightsPage/UpcomingFlightsPage.jsx
@@ -24,7 +24,7 @@ const UpcomingFlightsPage = () => {
       <Box w="100vw" h="100vh" positon="relative">
         <NavBar />
         <Heading textAlign="center">Upcoming Check Ins</Heading>
-        <VStack h="60px" maxW="500px" m="auto" px="10px" mt="30px">
+        <VStack h="60px" maxW="700px" m="auto" px="10px" mt="30px">
           {flights.map((flight, index) => {
             return (
               <>
