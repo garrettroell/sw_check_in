374d44f979c2f4ca42abb570fb32d267af50e894
Garrett Roell <garrettroell@gmail.com>
2025-05-15 00:13:58 -1000

    added args to the puppeteer launches

--- server/helpers/puppeteerControllers/getFlightsFromSWController.js
+++ server/helpers/puppeteerControllers/getFlightsFromSWController.js
@@ -29,6 +29,7 @@ async function getFlightsFromSWController({
   const browser = await puppeteer.launch({
     headless: true,
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
   await page.goto(url, { timeout: 0 });
